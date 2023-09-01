e1e08e01c21def6a8872608b9473fb3e5526f681
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-09-01 11:13:43 +0530

    Consumer Config

--- DeliveryBoy/src/main/java/com/deliveryBoy/DeliveryBoy/config/AppConstants.java
+++ DeliveryBoy/src/main/java/com/deliveryBoy/DeliveryBoy/config/AppConstants.java
@@ -2,4 +2,5 @@ package com.deliveryBoy.DeliveryBoy.config;
 
 public class AppConstants {
     public static final String LOCATION_TOPIC_NAME="location-update-topic";
+    public static final String GROUP_ID="group-1";
 }
---
+++ EndUser/src/main/java/com/endUser/EndUser/config/KafkaConfig.java
@@ -0,0 +1,14 @@
+package com.endUser.EndUser.config;
+
+import com.endUser.EndUser.constants.AppConstant;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    @KafkaListener(topics = AppConstant.LOCATION_UPDATE_TOPIC,groupId = AppConstant.GROUP_ID)
+    public void updatedLocation(String value){
+        System.out.println(value);
+    }
+}
